d9b0ac566607f6785bdc4e16a1fcf32186f95a68
luiscondori98 <lcondori11@hotmail.com>
2025-02-05 16:54:37 -0500

    codigo correlatido empleadoService

--- src/main/java/com/cibertec/projectefsrt/repositories/EmpleadoRepository.java
+++ src/main/java/com/cibertec/projectefsrt/repositories/EmpleadoRepository.java
@@ -1,7 +1,12 @@
 package com.cibertec.projectefsrt.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cibertec.projectefsrt.entities.Empleado;
 
-public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {}
+public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
+	
+	Optional<Empleado> findTopByOrderByCodEmpleadoDesc();
+}
